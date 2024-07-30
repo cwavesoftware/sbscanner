afec8a6eabb182b98c708947cc801bf0096f1885
cosmin-craciun-superbet <cosmin.craciun@happening.xyz>
2024-07-30 12:01:51 +0300

    removing faraday

--- faraday/faraday/settings/exceptions.py
+++
@@ -1,8 +0,0 @@
-class MissingConfigurationError(Exception):
-    """Raised when setting configuration is missing"""
-    pass
-
-
-class InvalidConfigurationError(Exception):
-    """Raised when setting configuration is invalid"""
-    pass
