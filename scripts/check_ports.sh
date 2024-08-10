477af27185b024c7009664a5e74119ee4adcd3f2
cosmin-craciun-superbet <cosmin.craciun@happening.xyz>
2024-08-09 18:58:09 -0700

    sending notif for on demand scan

--- scripts/check_ports.sh
+++ scripts/check_ports.sh
@@ -2,6 +2,7 @@
 
 send_notif=$1
 faraday_workspace=$2
+ports_to_skip_notifications=$3
 
 echo && echo "INFO: Checking ports ..."
 last_host_port_file=out/host_port.txt
@@ -9,71 +10,69 @@ rm -f $last_host_port_file
 ports_not_allowed_file=out/ports_not_allowed.txt
 rm -f $ports_not_allowed_file
 
-faraday-cli service list -w $faraday_workspace -j | jq -r '.[] | [.value.host.ip, .value.port] | join(":")' | sort > $last_host_port_file
+faraday-cli service list -w $faraday_workspace -j | jq -r '.[] | [.value.host.ip, .value.port] | join(":")' | sort >$last_host_port_file
 
-while IFS= read -r line
-do
-    redis-cli -h $REDIS_SERVER DEL $line:fixed
-    if [ $(redis-cli -h $REDIS_SERVER --raw EXISTS "$line:allowed") -eq 1 ] && [ $(redis-cli -h $REDIS_SERVER --raw GET "$line:allowed") == "true" ]; then
-        echo "$line is allowed"
-    else
-        echo $line >> $ports_not_allowed_file
-    fi
-done < "$last_host_port_file"
+while IFS= read -r line; do
+	redis-cli -h $REDIS_SERVER DEL $line:fixed
+	if [ $(redis-cli -h $REDIS_SERVER --raw EXISTS "$line:allowed") -eq 1 ] && [ $(redis-cli -h $REDIS_SERVER --raw GET "$line:allowed") == "true" ]; then
+		echo "$line is allowed"
+	else
+		echo $line >>$ports_not_allowed_file
+	fi
+done <"$last_host_port_file"
 
 if [ -s "$ports_not_allowed_file" ]; then
-    echo "INFO: Not allowed ports saved into $ports_not_allowed_file:" && cat $ports_not_allowed_file
-    new_ports_file=out/new_ports.txt
-    comm -23 $ports_not_allowed_file second_last_host_port.txt > $new_ports_file
+	echo "INFO: Not allowed ports saved into $ports_not_allowed_file:" && cat $ports_not_allowed_file
+	new_ports_file=out/new_ports.txt
+	comm -23 $ports_not_allowed_file second_last_host_port.txt >$new_ports_file
 
-    echo "INFO: Filtering ports we don't want to be notified about ..."
-    cp $new_ports_file $new_ports_file.copy
-    rg=$(echo $3 | sed -E 's/,/$|:/pg' | sed -En 's/^/:/pg' | sed -En 's/$/\$/pg' | head -n 1)
-    grep -v -E "$rg" $new_ports_file.copy > $new_ports_file
+	echo "INFO: Filtering ports we don't want to be notified about ..."
+	cp $new_ports_file $new_ports_file.copy
+	rg=$(echo $ports_to_skip_notifications | sed -E 's/,/$|:/pg' | sed -En 's/^/:/pg' | sed -En 's/$/\$/pg' | head -n 1)
+	grep -v -E "$rg" $new_ports_file.copy >$new_ports_file
 
-    if [ "$send_notif" == "1" ]; then
-        if [ -s "$new_ports_file" ]; then
-            # We send notifications only for new ports discovered
-            echo "INFO: Sending notification ..."
-            if [ $(uname) == "Linux" ]; then
-                sed -i '1s/^/New ports discovered:\n/' $new_ports_file
-            fi
-            if [ $(uname) == "Darwin" ]; then
-                sed -i '' '1s/^/New ports discovered:\n/' $new_ports_file
-            fi
-            notify -nc -pc ./notify-config.yaml -i $new_ports_file --bulk --cl 1000
-        else
-            echo "No new ports detected" 
-            echo "No new ports detected" | notify -nc -pc ./notify-config.yaml
-        fi
-    else
-        echo "Notifications are OFF"
-    fi
+	if [ "$send_notif" == "1" ]; then
+		if [ -s "$new_ports_file" ]; then
+			# We send notifications only for new ports discovered
+			echo "INFO: Sending notification ..."
+			if [ $(uname) == "Linux" ]; then
+				sed -i '1s/^/New ports discovered:\n/' $new_ports_file
+			fi
+			if [ $(uname) == "Darwin" ]; then
+				sed -i '' '1s/^/New ports discovered:\n/' $new_ports_file
+			fi
+			notify -nc -pc ./notify-config.yaml -i $new_ports_file --bulk --cl 1000
+		else
+			echo "No new ports detected"
+			echo "No new ports detected" | notify -nc -pc ./notify-config.yaml
+		fi
+	else
+		echo "Notifications are OFF"
+	fi
 fi
 
 echo "INFO: Looking for ports that were fixed ..."
 fixed_ports_file=out/fixed_ports.txt
-comm -23 second_last_host_port.txt $last_host_port_file > $fixed_ports_file
+comm -23 second_last_host_port.txt $last_host_port_file >$fixed_ports_file
 if [ -s "$fixed_ports_file" ]; then
-    echo "INFO: Some ports were fixed (closed):" && cat $fixed_ports_file 
+	echo "INFO: Some ports were fixed (closed):" && cat $fixed_ports_file
 
-    while IFS= read -r line
-    do
-        redis-cli -h $REDIS_SERVER SET "$line:fixed" true
-    done < "$fixed_ports_file"
+	while IFS= read -r line; do
+		redis-cli -h $REDIS_SERVER SET "$line:fixed" true
+	done <"$fixed_ports_file"
 
-    if [ "$send_notif" == "1" ]; then
-        echo "INFO: Sending notification ..."
-        if [ $(uname) == "Linux" ]; then
-            sed -i '1s/^/Fixed (closed) ports:\n/' $fixed_ports_file
-        fi
-        if [ $(uname) == "Darwin" ]; then
-            sed -i '' '1s/^/Fixed (closed) ports:\n/' $fixed_ports_file
-        fi
-        # notify -nc -pc ./notify-config.yaml -i $fixed_ports_file --bulk --cl 1000
-    fi
+	if [ "$send_notif" == "1" ]; then
+		echo "INFO: Sending notification ..."
+		if [ $(uname) == "Linux" ]; then
+			sed -i '1s/^/Fixed (closed) ports:\n/' $fixed_ports_file
+		fi
+		if [ $(uname) == "Darwin" ]; then
+			sed -i '' '1s/^/Fixed (closed) ports:\n/' $fixed_ports_file
+		fi
+		# notify -nc -pc ./notify-config.yaml -i $fixed_ports_file --bulk --cl 1000
+	fi
 else
-    echo "INFO: No ports were fixed"
+	echo "INFO: No ports were fixed"
 fi
 
 exit 0
--- scripts/sbscanner.sh
+++ scripts/sbscanner.sh
@@ -2,15 +2,15 @@
 
 echo "INFO: Checking for necessary tools ..."
 if which masscan && which faraday-cli && which redis-cli && which jq; then
-    echo "INFO: Tools installed, great"
-else 
-    echo "ERROR: Some tools were not found, make sure it's installed and available in PATH"
-    exit 1
+	echo "INFO: Tools installed, great"
+else
+	echo "ERROR: Some tools were not found, make sure it's installed and available in PATH"
+	exit 1
 fi
 
 if [ "$#" -lt 5 ]; then
-    echo "Usage: bash $0 <ips_input_file> <masscan_rate> <ports> <faraday_workspace> <make_diff> [ports_to_skip_notifications]"
-    exit 1
+	echo "Usage: bash $0 <ips_input_file> <masscan_rate> <ports> <faraday_workspace> <make_diff> [ports_to_skip_notifications]"
+	exit 1
 fi
 
 faraday_workspace=$4
@@ -22,18 +22,18 @@ echo "INFO: The Following IP addresses/ranges will be scanned:"
 cat targets/$1
 
 mkdir out 2>/dev/null
-outfile=out/masscan_report_`date +%s%3N`.json
+outfile=out/masscan_report_$(date +%s%3N).json
 
 echo "INFO: Running masscan ..."
 masscan --rate $2 -iL targets/$1 -p T:$3 -oJ $outfile
 if [ $? -gt 0 ]; then
-    echo "ERROR: masscan raised an error"
-    exit 1
+	echo "ERROR: masscan raised an error"
+	exit 1
 fi
 
 if [ ! -s "$outfile" ]; then
-    echo "ERROR: masscan report file $outfile doesn't exist or is empty. Exiting ..."
-    exit 1
+	echo "ERROR: masscan report file $outfile doesn't exist or is empty. Exiting ..."
+	exit 1
 fi
 echo "INFO: Results saved to $outfile"
 
@@ -42,44 +42,49 @@ bash nmap_wrapper.sh $outfile
 echo && echo "INFO: Importing nmap results into faraday ..."
 faraday-cli auth -f $FARADAY_URL -u $FARADAY_USER -p "$FARADAY_PASSWORD"
 if [ $? -gt 0 ]; then
-    echo "ERROR: Couldn't authenticate to faraday server"
-    exit 1
+	echo "ERROR: Couldn't authenticate to faraday server"
+	exit 1
 fi
 
 if [ "$make_diff" == "true" ]; then
-    faraday-cli service list -w $faraday_workspace -j | jq -r '.[] | [.value.host.ip, .value.port] | join(":")' | sort > second_last_host_port.txt
-    # Delete last scan from faraday and replace it with most recent scan
-    faraday-cli workspace delete $faraday_workspace
+	faraday-cli service list -w $faraday_workspace -j | jq -r '.[] | [.value.host.ip, .value.port] | join(":")' | sort >second_last_host_port.txt
+	# Delete last scan from faraday and replace it with most recent scan
+	faraday-cli workspace delete $faraday_workspace
+else # is probably on demand
+	cat ../static/hosts_header.txt >tmp
+	faraday-cli host list -w $faraday_workspace | grep "$(cat targets/$1 | tr '\n' '|' | sed 's/|$//g')" -E >>tmp
+	cat ../static/services_header.txt >>tmp
+	faraday-cli service list -w $faraday_workspace | grep "$(cat targets/$1 | tr '\n' '|' | sed 's/|$//g')" -E >>tmp
 fi
 
 faraday-cli workspace create $faraday_workspace
 faraday-cli workspace select $faraday_workspace
 
-while IFS= read -r report
-do
-    faraday-cli tool report $report
+while IFS= read -r report; do
+	faraday-cli tool report $report
 done < <(ls -al out/nmap_report* | rev | cut -d " " -f1 | rev)
 
-hosts=`faraday-cli host list -w $faraday_workspace -j`
-services=`faraday-cli service list -w $faraday_workspace -j`
+hosts=$(faraday-cli host list -w $faraday_workspace -j)
+services=$(faraday-cli service list -w $faraday_workspace -j)
 
 if [ "$make_diff" == "true" ]; then
-    if [ `redis-cli -h $REDIS_SERVER --raw EXISTS "$faraday_workspace"_last_hosts` -gt 0 ]; then
-        sendnotif=1
-        redis-cli -h $REDIS_SERVER DEL "$faraday_workspace"_second_last_hosts
-        redis-cli -h $REDIS_SERVER COPY "$faraday_workspace"_last_hosts "$faraday_workspace"_second_last_hosts
-        echo "INFO: Redis $(echo $faraday_workspace)_last_hosts copied to $(echo $faraday_workspace)_second_last_hosts"
-    else
-        echo "INFO: $(echo $faraday_workspace)_last_hosts not found in redis"
-    fi
+	if [ $(redis-cli -h $REDIS_SERVER --raw EXISTS "$faraday_workspace"_last_hosts) -gt 0 ]; then
+		sendnotif=1
+		redis-cli -h $REDIS_SERVER DEL "$faraday_workspace"_second_last_hosts
+		redis-cli -h $REDIS_SERVER COPY "$faraday_workspace"_last_hosts "$faraday_workspace"_second_last_hosts
+		echo "INFO: Redis $(echo $faraday_workspace)_last_hosts copied to $(echo $faraday_workspace)_second_last_hosts"
+	else
+		echo "INFO: $(echo $faraday_workspace)_last_hosts not found in redis"
+	fi
 else
-    echo "make_diff = $make_diff, we don't compare with previous scan"
+	echo "make_diff = $make_diff, we don't compare with previous scan"
 fi
 
 redis-cli -h $REDIS_SERVER SET $(echo -n $faraday_workspace)_last_hosts "$(echo -n $hosts)" && echo "INFO: $(echo -n $faraday_workspace)_last_hosts saved in redis"
 
 if [ "$make_diff" == "true" ]; then
-    bash diff.sh $sendnotif $faraday_workspace
-    check_ports.sh $sendnotif $faraday_workspace $ports_to_skip_notifications
+	bash diff.sh $sendnotif $faraday_workspace
+	check_ports.sh $sendnotif $faraday_workspace $ports_to_skip_notifications
 fi
 exit 0
+
